e347ef23d8d3597a8cb5a5fd75ee644df4a191d9
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-06-16 03:21:45 +0200

    Add form

---
+++ RandomNumberGenerator/RandomNumberGenerratorForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace RandomNumberGenerator
+{
+    partial class RandomNumberGenerratorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fromLabel = new System.Windows.Forms.Label();
+            this.toLabel = new System.Windows.Forms.Label();
+            this.optionsGroupBox = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultListBox = new System.Windows.Forms.ListBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.fromNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.toNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.amountNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.optionsGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fromNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.amountNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fromLabel
+            // 
+            this.fromLabel.AutoSize = true;
+            this.fromLabel.Location = new System.Drawing.Point(9, 25);
+            this.fromLabel.Name = "fromLabel";
+            this.fromLabel.Size = new System.Drawing.Size(21, 13);
+            this.fromLabel.TabIndex = 0;
+            this.fromLabel.Text = "Od";
+            // 
+            // toLabel
+            // 
+            this.toLabel.AutoSize = true;
+            this.toLabel.Location = new System.Drawing.Point(9, 53);
+            this.toLabel.Name = "toLabel";
+            this.toLabel.Size = new System.Drawing.Size(21, 13);
+            this.toLabel.TabIndex = 2;
+            this.toLabel.Text = "Do";
+            // 
+            // optionsGroupBox
+            // 
+            this.optionsGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.optionsGroupBox.Controls.Add(this.copyButton);
+            this.optionsGroupBox.Controls.Add(this.amountNumericUpDown);
+            this.optionsGroupBox.Controls.Add(this.toNumericUpDown);
+            this.optionsGroupBox.Controls.Add(this.fromNumericUpDown);
+            this.optionsGroupBox.Controls.Add(this.generateButton);
+            this.optionsGroupBox.Controls.Add(this.label1);
+            this.optionsGroupBox.Controls.Add(this.fromLabel);
+            this.optionsGroupBox.Controls.Add(this.toLabel);
+            this.optionsGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.optionsGroupBox.Name = "optionsGroupBox";
+            this.optionsGroupBox.Size = new System.Drawing.Size(232, 147);
+            this.optionsGroupBox.TabIndex = 4;
+            this.optionsGroupBox.TabStop = false;
+            this.optionsGroupBox.Text = "Ustawienia";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ilość";
+            // 
+            // resultListBox
+            // 
+            this.resultListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultListBox.FormattingEnabled = true;
+            this.resultListBox.Location = new System.Drawing.Point(12, 169);
+            this.resultListBox.Name = "resultListBox";
+            this.resultListBox.Size = new System.Drawing.Size(232, 95);
+            this.resultListBox.TabIndex = 5;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(44, 112);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(75, 23);
+            this.generateButton.TabIndex = 6;
+            this.generateButton.Text = "Generuj";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // fromNumericUpDown
+            // 
+            this.fromNumericUpDown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fromNumericUpDown.Location = new System.Drawing.Point(44, 23);
+            this.fromNumericUpDown.Name = "fromNumericUpDown";
+            this.fromNumericUpDown.Size = new System.Drawing.Size(178, 20);
+            this.fromNumericUpDown.TabIndex = 7;
+            this.fromNumericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // toNumericUpDown
+            // 
+            this.toNumericUpDown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.toNumericUpDown.Location = new System.Drawing.Point(44, 51);
+            this.toNumericUpDown.Name = "toNumericUpDown";
+            this.toNumericUpDown.Size = new System.Drawing.Size(178, 20);
+            this.toNumericUpDown.TabIndex = 8;
+            this.toNumericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // amountNumericUpDown
+            // 
+            this.amountNumericUpDown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.amountNumericUpDown.Location = new System.Drawing.Point(44, 77);
+            this.amountNumericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.amountNumericUpDown.Name = "amountNumericUpDown";
+            this.amountNumericUpDown.Size = new System.Drawing.Size(178, 20);
+            this.amountNumericUpDown.TabIndex = 9;
+            this.amountNumericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // copyButton
+            // 
+            this.copyButton.Location = new System.Drawing.Point(147, 112);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(75, 23);
+            this.copyButton.TabIndex = 10;
+            this.copyButton.Text = "Kopiuj";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
+            // 
+            // RandomNumberGenerratorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 275);
+            this.Controls.Add(this.resultListBox);
+            this.Controls.Add(this.optionsGroupBox);
+            this.MinimumSize = new System.Drawing.Size(207, 314);
+            this.Name = "RandomNumberGenerratorForm";
+            this.Text = "Generator liczb losowych";
+            this.optionsGroupBox.ResumeLayout(false);
+            this.optionsGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fromNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.amountNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fromLabel;
+        private System.Windows.Forms.Label toLabel;
+        private System.Windows.Forms.GroupBox optionsGroupBox;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox resultListBox;
+        private System.Windows.Forms.NumericUpDown amountNumericUpDown;
+        private System.Windows.Forms.NumericUpDown toNumericUpDown;
+        private System.Windows.Forms.NumericUpDown fromNumericUpDown;
+        private System.Windows.Forms.Button copyButton;
+    }
+}
+
---
+++ RandomNumberGenerator/RandomNumberGenerratorForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PDCore.Extensions;
+using PDCore.Helpers;
+
+namespace RandomNumberGenerator
+{
+    /// <summary>
+    /// Klasa częściowa, jest w kilku plikach, by zwiększyć czytelność i nie wyświetlać zbędnych informacje w ciele klasy.
+    /// </summary>
+    public partial class RandomNumberGenerratorForm : Form
+    {
+        /// <summary>
+        /// Konstruktor odpowiada za przygotowanie danych, przypisanie zmiennych itd.
+        /// </summary>
+        public RandomNumberGenerratorForm()
+        {
+            InitializeComponent();
+
+            fromNumericUpDown.Minimum = int.MinValue;
+            fromNumericUpDown.Maximum = int.MaxValue;
+
+            toNumericUpDown.Minimum = fromNumericUpDown.Minimum;
+            toNumericUpDown.Maximum = fromNumericUpDown.Maximum;
+
+            amountNumericUpDown.Maximum = fromNumericUpDown.Maximum;
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            var result = GetResult();
+
+            ShowResult(result);
+        }
+
+        private void ShowResult(int[] result)
+        {
+            resultListBox.Items.Clear();
+
+            if (result == null)
+                return;
+
+            result.ForEach(x => resultListBox.Items.Add(x));
+        }
+
+        private int[] GetResult()
+        {
+            int from = fromNumericUpDown.GetValueInt();
+
+            int to = toNumericUpDown.GetValueInt();
+
+            int amount = amountNumericUpDown.GetValueInt();
+
+
+            int[] result = null;
+
+            try
+            {
+                result = PDCore.Helpers.RandomNumberGenerator.Next(from, to, amount);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Błąd");
+            }
+
+            return result;
+        }
+
+        private void Copy()
+        {
+            if (resultListBox.Items.Count > 0)
+            {
+                string result = string.Join(", ", resultListBox.Items.Cast<int>());
+
+                Clipboard.SetText(result);
+            }
+        }
+
+        private void copyButton_Click(object sender, EventArgs e)
+        {
+            Copy();
+        }
+    }
+}
