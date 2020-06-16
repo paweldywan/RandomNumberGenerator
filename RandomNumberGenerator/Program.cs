b1ca64f3e14210db5a4e88c4bc172c2273d74549
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-06-16 03:21:03 +0200

    Development

--- RandomNumberGenerator/Program.cs
+++ RandomNumberGenerator/Program.cs
@@ -16,7 +16,7 @@ namespace RandomNumberGenerator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new RandomNumberGenerratorForm());
         }
     }
 }
